628ae22f59e2dff0b96a14e3d4be148387c5051a
shejanNizam <nizam.shejan11@gmail.com>
2024-03-18 22:47:24 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className="m-24">
         <Header />
         <h1 className="text-6xl text-purple-400 text-center">
-          Hot Hot Cold Coffee Items: {coffees?.length}
+          Hot Cold Coffee Items: {coffees?.length}
         </h1>
         <div className="grid md:grid-cols-2 gap-4 my-8">
           {coffees?.map((coffee) => (
