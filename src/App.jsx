967fb5ce9b7f6fe1d390909428e933421785f1d9
shejanNizam <nizam.shejan11@gmail.com>
2024-10-27 23:36:31 +0600

    read operation add

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <div className="m-20">
         <h1 className="text-6xl text-center my-20 text-purple-600">
-          Hot Hot Cold Coffee: {coffees.length}
+          Hot Cold Cold Coffee: {coffees.length}
         </h1>
         <div className="grid md:grid-cols-2 gap-4">
           {coffees.map((coffee) => (
